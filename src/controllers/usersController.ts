27b135f8a5bd8c133e2ac5e7205e51592c9a2480
VictorAPuntel <victor.a.puntel@gmail.com>
2025-08-25 22:25:39 -0300

    fix(users): Creating validation for the create route

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,7 +1,26 @@
 import { Request, Response } from 'express'
+import { z } from 'zod'
+import { UserRole } from '@prisma/client'
 
 class UsersController {
   async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      name: z.string().trim().min(2, { message: 'Name is required' }),
+      email: z
+        .string()
+        .trim()
+        .email({ message: 'Invalid email' })
+        .toLowerCase(),
+      password: z
+        .string()
+        .min(6, { message: 'The password must have minimum 6 digits' }),
+      role: z
+        .enum([UserRole.employee, UserRole.manager])
+        .default(UserRole.employee),
+    })
+
+    const { name, email, password, role } = bodySchema.parse(request.body)
+
     return response.json({ message: 'ok' })
   }
 }
