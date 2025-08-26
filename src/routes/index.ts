295d4b4b3ccebb6b15e59410118fd88ea86a8be1
VictorAPuntel <victor.a.puntel@gmail.com>
2025-08-26 20:16:49 -0300

    fix(sessions): Creating the public routes for sessions

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import { usersRoutes } from './usersRoutes'
+import { sessionsRoutes } from './sessionsRoutes'
 
 const routes = Router()
 
 //Public Routes
 routes.use('/users', usersRoutes)
+routes.use('/sessions', usersRoutes)
 
 export { routes }
