808c1c9fc0e13d7ed2f71172ea52e693d6fd89d1
VictorAPuntel <victor.a.puntel@gmail.com>
2025-08-26 20:23:55 -0300

    fix(sessions): Creating validation for email or password

--- src/controllers/sessionsController.ts
+++ src/controllers/sessionsController.ts
@@ -1,7 +1,32 @@
 import { Request, Response } from 'express'
+import { z } from 'zod'
+import { prisma } from '@/database/prisma'
+import { appError } from '@/utils/AppError'
+import { compare } from 'bcrypt'
 
 class SessionsController {
-  async create(request: Request, response: Response) {}
+  async create(request: Request, response: Response) {
+    const bodySchema = z.object({
+      email: z.string().email({ message: 'Invalid email' }),
+      password: z.string(),
+    })
+
+    const { email, password } = bodySchema.parse(request.body)
+
+    const user = await prisma.user.findFirst({ where: { email } })
+
+    if (!user) {
+      throw new appError('Email or password is invalid', 401)
+    }
+
+    const passwordMatched = await compare(password, user.password)
+
+    if (!passwordMatched) {
+      throw new appError('Email or password is invalid', 401)
+    }
+
+    response.json()
+  }
 }
 
 export { SessionsController }
