69c4a4636f932115ffc3f73ce0f01962977ac0e8
VictorAPuntel <victor.a.puntel@gmail.com>
2025-08-26 21:59:15 -0300

    create: Creating verification to user for access especific routes

--- src/routes/refoundsRoutes.ts
+++ src/routes/refoundsRoutes.ts
@@ -1,9 +1,14 @@
 import { Router } from 'express'
 import { RefundsController } from '@/controllers/refundsController'
+import { verifyUserAuthorization } from '@/middlewares/verifyUserAuthorization'
 
 const refoundsRoutes = Router()
 const refundsController = new RefundsController()
 
-refoundsRoutes.post('/', refundsController.create)
+refoundsRoutes.post(
+  '/',
+  verifyUserAuthorization(['employee']),
+  refundsController.create
+)
 
 export { refoundsRoutes }
